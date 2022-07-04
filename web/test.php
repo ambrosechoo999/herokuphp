e1ad7de7de9f1ba1f5f636f03e22d0214a0d5799
Ambrose Choo <20008189@myrp.edu.sg>
2022-07-04 12:47:21 +0800

    Commit

--- web/test.php
+++ web/test.php
@@ -1,6 +1,8 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta name="viewport">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Heroku Test 9">
     <title> Test Page </title>
 </head>
 <body>
